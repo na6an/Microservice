e7243592068eaf389068499b1f2b5453283947ca
Nathan <jbyun6@gmail.com>
2020-07-29 10:34:03 -0400

    confirmed all working

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=na6an/api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run api --image=na6an/api --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/api  8000:80
+# kubectl expose deployment api --type=LoadBalancer --port=8000 --target-port=80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=na6an/api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag 795aa013d1ba $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
